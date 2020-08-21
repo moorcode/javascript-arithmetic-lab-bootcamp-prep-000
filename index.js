169754074c24fea1944ed550790e3e9d3f3146d7
Christina Poole <codenamedchris@gmail.com>
2020-08-21 16:54:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ function decrement(n){
     return n-=1;
 };
 function makeInt(string){
-  return 0;
+  num3 = parseInt(string, 10)
+  return num3;
 };
 function preserveDecimal(string){
   return 0;
